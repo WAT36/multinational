5979125f769f5c1bd2a30867fcc647a78c4d76d8
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-30 23:20:44 +0900

    コンソール入力 Java

---
+++ java/Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Input{
+    public static void main(String args[]){
+
+        //Scannerのインスタンスを生成
+        Scanner sc = new Scanner(System.in);
+
+        //String型で１行読み込む
+        String s = sc.nextLine();
+
+        System.out.println("入力された値:"+s);
+    }
+}
--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,14 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 class Main{
-    public static void main(String args[]){
-        System.out.println(Arrays.toString(args));
-    }
+  public static void main(String args[]){
+
+    //Scannerのインスタンスを生成
+    Scanner sc = new Scanner(System.in);
+
+    //String型で１行読み込む
+    String s = sc.nextLine();
+
+    System.out.println("入力された値:"+s);
+  }
 }
