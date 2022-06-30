69b017934893dfdcb527a486e5a142a88484cff4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-30 23:03:50 +0900

    CLI Java

---
+++ java/CLI.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class CLI{
+    public static void main(String args[]){
+        System.out.println(Arrays.toString(args));
+    }
+}
---
+++ java/Main.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Main{
+    public static void main(String args[]){
+        System.out.println(Arrays.toString(args));
+    }
+}
