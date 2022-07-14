7b5aa1237b1819f890741e27ccaca19922cf39ea
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-07-14 22:48:49 +0900

    文字列含まれるか　Java

---
+++ java/Contain.java
@@ -0,0 +1,8 @@
+class Contain{
+    public static void main(String args[]){
+        String s = "apple,banana,cherry,durian";
+        System.out.println(s.contains("banana"));
+
+        System.out.println(s.contains("grape"));     
+    }
+}
