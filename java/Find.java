51241a7e82e7c169f0ac415b1b069d458c73a613
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-07-19 23:19:52 +0900

    文字列Find  java

---
+++ java/Find.java
@@ -0,0 +1,8 @@
+class Find{
+    public static void main(String args[]){
+        String s = "apple,banana,cherry,durian";
+        System.out.println(s.indexOf("banana"));
+
+        System.out.println(s.indexOf("banana",10));        
+    }
+}
