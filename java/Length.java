6d1e6c0833ed1375e5b1915c03724e4e2e06d671
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-07-22 20:22:44 +0900

    文字列の長さ Java

---
+++ java/Length.java
@@ -0,0 +1,11 @@
+class Length{
+    public static void main(String args[]){
+        String a = "ab";
+        String b = "11223344";
+        String c = "**************";
+
+        System.out.println(a.length());
+        System.out.println(b.length());
+        System.out.println(c.length());
+    }
+}
