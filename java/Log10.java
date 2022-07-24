dd223416d31db7546c0682b8e8c061ebae1953f7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-07-24 19:57:23 +0900

    常用対数 java

---
+++ java/Log10.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+class Log10{
+    public static void main(String args[]){
+        int a = 100;
+        int b = 500;
+
+        System.out.println(Math.log10(a));
+        System.out.println(Math.log10(b));
+    }
+}
