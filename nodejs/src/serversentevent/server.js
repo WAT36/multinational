bb25611b8d893699bf41085de6594668d2bf7b30
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-04-01 23:29:11 +0900

    path変更

--- nodejs/src/serversentevent/server.js
+++ nodejs/src/serversentevent/server.js
@@ -8,7 +8,7 @@ app.use((_, res, next) => {
   next();
 });
 
-app.get("/events", (req, res) => {
+app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
