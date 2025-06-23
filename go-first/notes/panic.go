2427c82cf3161aadff13ab99bb85acc28c0f1615
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-06-23 23:55:08 +0900

    エラーハンドリングの追加: パニックを回復し、エラーメッセージを表示する機能を実装

--- go-first/notes/panic.go
+++ go-first/notes/panic.go
@@ -11,6 +11,23 @@ func PanicRecover() {
 	}()
 
 	fmt.Println("処理開始")
-	panic("パニック発生！")
-	fmt.Println("ここは実行されません")
+
+	// エラーハンドリング例
+	err := func() (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = fmt.Errorf("エラー発生: %v", r)
+			}
+		}()
+		// ここでパニックを発生させる
+		panic("パニック発生！")
+		// ここは実行されません
+		return nil
+	}()
+
+	if err != nil {
+		fmt.Println("エラーを検知:", err)
+	} else {
+		fmt.Println("正常終了")
+	}
 }
